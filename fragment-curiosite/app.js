ed3e04e698260c320aff9eb2ed4e3b82153029b0
Jlrycoder <leroyjeanne-@orange.fr>
2026-08-09 16:15:47 +0200

    Remove device orientation event listener for fragment

--- fragment-curiosite/app.js
+++ fragment-curiosite/app.js
@@ -1,17 +1,1 @@
-const fragment = document.querySelector(".fragment");
 
-if (fragment) {
-    window.addEventListener("deviceorientation", function (event) {
-
-        const beta = event.beta || 0;
-        const gamma = event.gamma || 0;
-
-        const rotationX = Math.max(-10, Math.min(10, beta / 6));
-        const rotationY = Math.max(-10, Math.min(10, gamma / 3));
-
-        fragment.style.transform =
-            `perspective(1000px)
-             rotateX(${rotationX}deg)
-             rotateY(${rotationY}deg)`;
-    });
-}
